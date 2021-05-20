fbb6fc0293da5d9aa5582f9b1131e4c45a35c028
Student <student@PNCLB13-02.pnc.lan>
2021-05-20 23:21:45 +0700

    day1

--- server.js
+++ server.js
@@ -3,9 +3,17 @@ const app =express();
 
 app.listen(process.env.PORT || 5000, ()=>console.log("server running..."));
 
+
 app.use(express.json());
 app.use(express.urlencoded());
 app.use(express.static('public'));
 
+let users=[
+    {name:'unknow'},
+    {name:'sreyhieb'},
+]
 
-app.get('/',(req,res)=>res.send("Hello project"));
+app.get('/users',(req,res)=>{
+    res.send(users);
+});
+
