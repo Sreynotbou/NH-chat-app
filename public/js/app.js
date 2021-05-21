f1834ac734d1ccc7715f527c342704fb17de543c
Student <student@PNCLB13-02.pnc.lan>
2021-05-21 10:10:12 +0700

    day2

--- public/js/app.js
+++ public/js/app.js
@@ -1,32 +1,26 @@
-
 function displayText(resposne){
-   let texts =resposne.data;
-
-}
-function send(event){
-   const chatsend= document.querySelector("#text").value;
-   const ul =document.querySelector('ul');
-   const li =document.createElement('li');
-   li.textContent=chatsend;
-   li.style.color="blue";
-   ul.appendChild(li);
-
-   // for put name user into using chat
-   // const name =prompt('what is your name?');
-   // appendMessage('you joined');
-
-let text= {text:chatsend};
-      const url = "";
-       axios
-       .post (url,text)
-       .then(displayText)
-}
-function loadData(){
-   const url = "";
-   axios
-   .post (url)
-   .then(displayText)
-}
-const btnSend =document.querySelector('button');
-btnSend.addEventListener('click',send);
-loadData();
+    let texts =resposne.data;
+ }
+ function send(event){
+    const chatsend= document.querySelector("#text").value;
+    const ul =document.querySelector('ul');
+    const li =document.createElement('li');
+    li.textContent=chatsend;
+    li.style.color="blue";
+    ul.appendChild(li);
+ 
+ let text= {text:chatsend};
+       const url = "http://localhost:5000/users/";
+        axios
+        .post (url,text)
+        .then(displayText)
+ }
+ function loadData(){
+    const url = "http://localhost:5000/users/";
+    axios
+    .post (url)
+    .then(displayText)
+ }
+ const btnSend =document.querySelector('button');
+ btnSend.addEventListener('click',send);
+ loadData();
--- server.js
+++ server.js
@@ -9,11 +9,17 @@ app.use(express.urlencoded());
 app.use(express.static('public'));
 
 let users=[
-    {name:'unknow'},
-    {name:'sreyhieb'},
+    {id:1, name:'unknow' ,message:"test"},
+    {id:2, name:'sreyhieb',message:"Hello world"},
 ]
 
 app.get('/users',(req,res)=>{
     res.send(users);
 });
+
+app.post('/users',(req,res)=>{
+    let newUsers =req.body;
+    users.push(newUsers);
+    res.send(newUsers);
+})
 
