81c11a01640e977d92f0a6357ccd6e5ba074f91e
Student <student@PNCLB13-02.pnc.lan>
2021-05-30 23:37:51 +0700

    update

--- public/js/app.js
+++ public/js/app.js
@@ -1,6 +1,6 @@
 
-// const url = "https://nh-chat-app.herokuapp.com/message";
-const url = "http://localhost:5000/message";
+const url = "https://nh-chat-app.herokuapp.com/message";
+// const url = "http://localhost:5000/message";
 
 function resqustUser(respone) {
   let users = respone.data;
